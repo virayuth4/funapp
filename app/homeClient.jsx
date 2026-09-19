74408408d9aca3252eb48d3668b63672ccd24928
virayuth <virayuthlim2000@gmail.com>
2026-09-19 10:45:16 +0700

    added home navigation

--- app/homeClient.jsx
+++ app/homeClient.jsx
@@ -13,6 +13,7 @@ import RollHistory from "./Components/rollHistory";
 import { getPusherClient } from "@/lib/pusherClient";
 import SessionShareModal from "./Components/sessionShareModal";
 import formattedCountdown from "@/lib/formttedCountdown";
+import { HomeNavigation } from "./Components/navigation";
 
 const TYPE_FIELD = "category";
 
@@ -700,6 +701,8 @@ const requestSpin = async () => {
 }, [sessionId]);
 
   return (
+    <>
+    <HomeNavigation/>
     <main className="min-h-screen bg-[#0d0f12] text-neutral-100 flex flex-col items-center relative overflow-hidden select-none">
       <SpinSounds ref={spinSoundsRef}  muted={isMuted} />
       <style jsx global>{`
@@ -975,5 +978,6 @@ const requestSpin = async () => {
         }}
       />
     </main>
+    </>
   );
 }
