58ef31ade8af7b5a238b5700729d3bba14fe8f88
virayuth <virayuthlim2000@gmail.com>
2026-09-20 21:56:49 +0700

    card ui

--- app/Components/cafeListViews.jsx
+++ app/Components/cafeListViews.jsx
@@ -258,7 +258,7 @@ function CafeRow({ rank, cafe, accent, onClick }) {
 
 {/* Price */}
  {cafe.price_range && (
-    <p className="text-xs text-white pt-1.5">
+    <p className="text-xs text-white pt-1.5 px-4">
       <PriceRangeDisplay priceRange={cafe.price_range} />
     </p>
   )}
